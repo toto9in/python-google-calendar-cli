4fd68636f7be353fe998cb69e1dea95eb22b9c41
Antonio <aaba03@gmail.com>
2024-04-08 10:52:15 -0300

    add suport to create simple events and show description of events

--- calendar_utils.py
+++ calendar_utils.py
@@ -1,6 +1,5 @@
 from datetime import datetime, timezone, timedelta
 import os.path
-
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
@@ -97,6 +96,42 @@ def list_events_today(credentials):
   except HttpError as error:
     print(f"An error occurred: {error}")
   
+def create_events(
+    event_name: str,
+    start_date_time: str,
+    end_date_time: str,
+    description: str ,
+):
+  creds = get_credentials()
+  service = build("calendar", "v3", credentials=creds)
+  time_zone = get_time_zone()
+  if time_zone is None:
+    print("Time zone not found.")
+    return
+  event = {
+    "summary": event_name,
+    "start": {"dateTime": start_date_time, "timeZone": get_time_zone()},
+    "end": {"dateTime": end_date_time , "timeZone": get_time_zone()},
+  }
+  if description != "":
+    event["description"] = description
+
+
+
+  event = service.events().insert(calendarId='primary', body=event).execute()
+  print(f"Event created with name: {event.get('summary')} and start date: {event.get('start').get('dateTime')}")
+
+##preciso ver um jeito de pegar os dados do user quando ele se authenticar para pegar info como timezone etc
+
+def get_time_zone():
+  creds = get_credentials()
+  service = build("calendar", "v3", credentials=creds)
+  settings = service.settings().list().execute()
+  for setting in settings['items']:
+    if setting['id'] == 'timezone':
+      return setting['value']
+  return None
+
 
 
 if __name__ == "__main__":
--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from typing import Optional
 import typer
 from typing_extensions import Annotated
 from rich import print, box
-from calendar_utils import get_events
+from calendar_utils import get_events, create_events
 from rich.console import Console
 from rich.table import Table, Column
 
@@ -15,11 +15,17 @@ console = Console()
 
 @events_app.command("create", no_args_is_help=True)
 def events_create(event_name: Annotated[str, typer.Argument(help="Name of the event")],
-                  description: Annotated[str, typer.Argument(help="Description of the event")]):
+                  start_date_time: Annotated[str, typer.Argument(help="Start date of the event")],
+                  end_date_time: Annotated[str, typer.Argument(help="End date of the event")],
+                  description: Annotated[str, typer.Option(help="Description of the event")] = ""):
     """
     [bold green]Create[/bold green] a new event in Google Calendar
+
+    start_date and end_date must be in the format: yyyy-mm-ddThh:mm:ss
+    end_date must be greater than start_date (format: yyyy-mm-ddThh:mm:ss)
+    example of start_date and end_date: 2024-04-08T12:30:00
     """
-    print(f"Create event {event_name} with description {description}")
+    create_events(event_name=event_name, start_date_time=start_date_time, end_date_time=end_date_time, description=description)
 
 @events_app.command("show")
 def events_show(filter: Annotated[str, typer.Option(help="Filter to show events that occurs at the actual day or at the week.")] = "today"):
@@ -31,19 +37,32 @@ def events_show(filter: Annotated[str, typer.Option(help="Filter to show events 
         if not events:
             print("No events today.")
             return
-        table = Table("Summary", Column(header="Status", style="green"), "Date", title="Events of today", show_lines=True, box=box.HEAVY_HEAD) 
+        table = Table("Summary", Column(header="Status", style="green"), "Description", "Date", title="Events of today", show_lines=True, box=box.HEAVY_HEAD) 
         for event in events:
-            table.add_row(event['summary'], event['status'], event['start']['dateTime'])
+            if 'description' not in event:
+                event['description'] = "No description"
+            table.add_row(event['summary'], event['status'], event['description'], event['start']['dateTime'])
         console.print(table)
     else:
         events = get_events("week")
         if not events:
             print("No events at the week.")
             return
-        table = Table("Summary", Column(header="Status", style="green"), "Date", title="Events of the week", show_lines=True, box=box.HEAVY_HEAD) 
+        table = Table("Summary", Column(header="Status", style="green"), "Description", "Date", title="Events of the week", show_lines=True, box=box.HEAVY_HEAD)
         for event in events:
-            table.add_row(event['summary'], event['status'], event['start']['dateTime'])
+            if 'description' not in event:
+                event['description'] = "No description"
+            table.add_row(event['summary'], event['status'], event['description'],  event['start']['dateTime'])
         console.print(table)
+
+@events_app.command("user-data")
+def user_data():
+    """
+    [bold green]Show[/bold green] user data
+    """
+
+    
+
 
 if __name__ == "__main__":
     app()
